90a4c2acc3e75724f95b43e123ae4149e3508773
Nikolaus Sucher <nikolaussucher@icloud.com>
2020-01-25 13:44:49 -0500

    recompiled html and pdf

--- pdf/make_pdf.sh
+++ pdf/make_pdf.sh
@@ -19,8 +19,8 @@ else {
       input=$prefix.pdf
       output=$prefix-low_res.pdf
       gs -sDEVICE=pdfwrite -dBATCH -dNOPAUSE -dCompatibilityLevel=1.4 -dOverrideICC -dColorConversionStrategy=/sRGB -dProcessColorModel=/DeviceRGB -dDetectDuplicateImages=true -dPDFSETTINGS=/ebook -sOutputFile=$output $input
-      cp $output ../docs/genetics_text.pdf
-      echo "Low-res-PDF generated and copied to the docs directory as genetics_text.pdf!"
+      cp $output ../docs/_main.pdf
+      echo "Low-res-PDF generated and copied to the docs directory as _main.pdf!"
     fi
   }
 fi
